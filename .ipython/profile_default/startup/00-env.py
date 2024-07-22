11bdf6dc2e0c106e2036eb3efe64c2b18becc58b
Stephen Vincent <stevevincent17@icloud.com>
2024-07-22 07:07:44 +0000

    completed analysis

---
+++ .ipython/profile_default/startup/00-env.py
@@ -0,0 +1,17 @@
+# *WARNING* Do not change this file as it may break your notebooks
+
+import os
+os.environ['HOME'] = '/home/ed426518-5eaa-450c-a90e-d1df1f73494d'
+os.environ['LC_CTYPE'] = 'en_US.utf-8'
+os.environ['JAVA_HOME'] = '/usr/lib/jvm/default-java'
+os.environ['TERM'] = 'xterm-256color'
+os.environ['PYTHONSTARTUP'] = '/home/ed426518-5eaa-450c-a90e-d1df1f73494d/.pythonstartup.py'
+os.environ['PYTHONANYWHERE_DOMAIN'] = 'anacondaapps.cloud'
+os.environ['PYTHONANYWHERE_SITE'] = 'nb.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_API_URL'] = 'https://assistant.anaconda.cloud'
+os.environ['ANACONDA_ASSISTANT_ENVIRONMENT_TYPE'] = 'cloud-notebooks-prod'
+os.environ['ANACONDA_PROJECTS_DIR'] = 'anaconda_projects'
+os.environ['http_proxy'] = 'http://proxy.server:3128'
+os.environ['https_proxy'] = 'http://proxy.server:3128'
+os.environ['no_proxy'] = 'localhost,127.0.0.1,localaddress,.localdomain.com,/var/run/docker.sock'
+os.environ['API_TOKEN'] = '988c67081c1f3ca42eb5a45d4d225bf2bd0fde32'
